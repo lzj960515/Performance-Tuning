846e46205f332276cf47365e43f49272d7cf2cc8
liaozijian <lzj960515@163.com>
2023-01-28 17:47:31 +0800

    对齐填充

--- jvm/my-jvm-test/src/com/my/jvm/test/allocate/JOLSample.java
+++ jvm/my-jvm-test/src/com/my/jvm/test/allocate/JOLSample.java
@@ -21,6 +21,9 @@ public class JOLSample {
         System.out.println();
         ClassLayout layout2 = ClassLayout.parseInstance(new A());
         System.out.println(layout2.toPrintable());
+        System.out.println();
+        ClassLayout layout3 = ClassLayout.parseInstance(new B());
+        System.out.println(layout3.toPrintable());
     }
     // -XX:+UseCompressedOops           默认开启的压缩所有指针
     // -XX:+UseCompressedClassPointers  默认开启的压缩对象头里的类型指针Klass Pointer
@@ -30,7 +33,14 @@ public class JOLSample {
         //4B Klass Pointer   如果关闭压缩-XX:-UseCompressedClassPointers或-XX:-UseCompressedOops，则占用8B
         int id;        //4B
         String name;   //4B  如果关闭压缩-XX:-UseCompressedOops，则占用8B
-        byte b;        //1B
+        byte b;        //1B  byte类型会自动填充到4字节，比如这个会填充3字节，如果后面还跟着byte a, 则填充2字节
         Object o;      //4B  如果关闭压缩-XX:-UseCompressedOops，则占用8B
+    }
+
+    public static class B {
+        //8B mark word
+        //4B Klass Pointer   如果关闭压缩-XX:-UseCompressedClassPointers或-XX:-UseCompressedOops，则占用8B
+        int id;        //4B
+        String name;   //4B  如果关闭压缩-XX:-UseCompressedOops，则占用8B
     }
 }
